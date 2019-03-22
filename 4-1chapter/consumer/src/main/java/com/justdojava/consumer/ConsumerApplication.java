5ed99c6ae5b0a4ee326c691e7378795fd5f2d200
江南一点雨 <wangsong0210@gmail.com>
2019-03-22 23:16:06 +0800

    服务注册与消费

---
+++ 4-1chapter/consumer/src/main/java/com/justdojava/consumer/ConsumerApplication.java
@@ -0,0 +1,20 @@
+package com.justdojava.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
