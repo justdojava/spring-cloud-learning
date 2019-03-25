c9ac716b7f342d29905f3f0490d7957e44cf2446
纯洁的微笑 <ityouknow@126.com>
2019-03-25 12:06:48 +0800

    add spring boot demo

---
+++ 2-3 Spring Boot 的设计理念和简单实践/hello/src/main/java/com/justdojava/hello/HelloController.java
@@ -0,0 +1,14 @@
+package com.justdojava.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello world";
+    }
+
+}
