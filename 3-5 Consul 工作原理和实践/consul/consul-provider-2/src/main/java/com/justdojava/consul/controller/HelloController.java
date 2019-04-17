00e5c0267760dc6e960361fffb0b81a94e61b714
纯洁的微笑 <ityouknow@126.com>
2019-04-17 18:59:43 +0800

    add

---
+++ 3-5 Consul 工作原理和实践/consul/consul-provider-2/src/main/java/com/justdojava/consul/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.justdojava.consul.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello consul two";
+    }
+}
