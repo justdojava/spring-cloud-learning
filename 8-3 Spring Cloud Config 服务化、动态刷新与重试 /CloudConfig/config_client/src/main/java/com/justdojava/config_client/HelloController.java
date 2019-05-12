572ad24fbf52eeec34137df3afbce94c36669919
江南一点雨 <wangsong0210@gmail.com>
2019-05-12 13:15:54 +0800

    Spring Cloud Config 服务化、动态刷新与重试

--- 8-3 Spring Cloud Config 服务化、动态刷新与重试 /CloudConfig/config_client/src/main/java/com/justdojava/config_client/HelloController.java
+++ 8-3 Spring Cloud Config 服务化、动态刷新与重试 /CloudConfig/config_client/src/main/java/com/justdojava/config_client/HelloController.java
@@ -1,6 +1,7 @@
 package com.justdojava.config_client;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date 2019/5/4 16:44
  */
 @RestController
+@RefreshScope
 public class HelloController {
     @Value("${javaboy}")
     String hello;
--- 8-3 Spring Cloud Config 服务化、动态刷新与重试 /CloudConfig/eureka/src/main/java/com/justdojava/eureka/EurekaApplication.java
+++ 8-3 Spring Cloud Config 服务化、动态刷新与重试 /CloudConfig/eureka/src/main/java/com/justdojava/eureka/EurekaApplication.java
@@ -2,8 +2,10 @@ package com.justdojava.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
 
     public static void main(String[] args) {
