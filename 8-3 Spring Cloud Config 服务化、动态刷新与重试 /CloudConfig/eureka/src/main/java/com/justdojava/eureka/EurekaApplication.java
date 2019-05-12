e37660ec0eac624de7e515e5051740ccd56dd3bc
江南一点雨 <wangsong0210@gmail.com>
2019-05-12 10:48:58 +0800

    8-3 未完

---
+++ 8-3 Spring Cloud Config 服务化、动态刷新与重试 /CloudConfig/eureka/src/main/java/com/justdojava/eureka/EurekaApplication.java
@@ -0,0 +1,13 @@
+package com.justdojava.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
