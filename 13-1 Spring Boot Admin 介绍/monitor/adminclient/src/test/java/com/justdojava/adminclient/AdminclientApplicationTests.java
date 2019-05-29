8efeb159ff7e61c57c4aa08a5ebd6b7165bb882b
江南一点雨 <wangsong0210@gmail.com>
2019-05-29 11:48:11 +0800

    Spring Boot Admin 介绍

---
+++ 13-1 Spring Boot Admin 介绍/monitor/adminclient/src/test/java/com/justdojava/adminclient/AdminclientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.justdojava.adminclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdminclientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
