f486f14845d0f005d7270de358a83afe6cdcb754
江南一点雨 <wangsong0210@gmail.com>
2019-05-29 19:12:30 +0800

    Spring Boot Admin 实践

---
+++ 13-2 Spring Boot Admin 实践/monitor/adminclient/src/main/java/com/justdojava/adminclient/AdminclientApplication.java
@@ -0,0 +1,13 @@
+package com.justdojava.adminclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminclientApplication.class, args);
+    }
+
+}
